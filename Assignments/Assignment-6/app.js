16e97a76f806a82f1c9fc911304b3cd75740ae88
Noorulainshoaib <ulainn400@gmail.com>
2023-06-09 19:24:19 +0500

    done

--- Assignments/Assignment-6/app.js
+++ Assignments/Assignment-6/app.js
@@ -3,19 +3,19 @@
   //Q-1 Write a program to take a number in a variable, do ther equired arithmetic to display the following result in your browser
   document.write("<h3>Task-1</h3>");
   var a=10;
-  document.write('Result: <br>The value of a is: '+ a);
+  document.write('Result: <br>The Value Of a Is: '+ a);
   document.write('<br>----------------------<br>');
-  document.write('<br>The value of ++a is: '+ ++a);
-  document.write('<br>Now the value of a is: '+ a);
+  document.write('<br>The Value Of ++a Is: '+ ++a);
+  document.write('<br>Now The Value Of a Is: '+ a);
   
-  document.write('<br> <br> The value of a++ is: '+ a++);
+  document.write('<br> <br> The Value Of a++ Is: '+ a++);
   document.write('<br> Now the value of a is: '+ a);
   
-  document.write('<br><br> The value of --a is: '+ --a);
-  document.write('<br> Now the value of a is: '+ a);
+  document.write('<br><br> The Value Of --a Is: '+ --a);
+  document.write('<br> Now The Value Of a Is: '+ a);
   
-  document.write('<br><br>The value of a--'+ a--);
-  document.write('<br>Now the value of a is: '+ a);
+  document.write('<br><br>The Value Of a--'+ a--);
+  document.write('<br>Now The Value Of a Is: '+ a);
 
   //Q-2 What will be the output in variables a, b & result after execution of the following script:
 //var a = 2, b = 1;
@@ -28,16 +28,16 @@
 
 document.write("<h3>Task-2</h3>");
 var a=2, b=1;
-document.write('<br>a is: '+ a);
-document.write('<br>b is: '+ b);
+document.write('<br>a Is: '+ a);
+document.write('<br>b Is: '+ b);
 var result = --a - --b + ++b + b--;
-document.write('<br>result is: '+ result +'<br><br>');
+document.write('<br>Result Is: '+ result +'<br><br>');
 
 
 //Q-3 Write a program that takes input a name from user & greet the user.
 document.write("<h3>Task-3</h3>");
-var inputname= prompt("Enter your name");
-alert("Have A Wonderful Day !"+ inputname);
+var inputName= prompt("Enter Your Name");
+alert("Have A Wonderful Day !"+ inputName);
 
 //4.
 
@@ -45,7 +45,7 @@ alert("Have A Wonderful Day !"+ inputname);
 //should be displayed by default.
 
 document.write("<h3>Task-4-5</h3>");
-var inputNum= prompt("Enter a number");
+var inputNum= prompt("Enter A Number");
 var inputNum= +inputNum;
 for(i=1;i<11;i++){
     multi=i*inputNum;
@@ -58,9 +58,9 @@ for(i=1;i<11;i++){
 
 document.write("<h3>Task-6</h3>");
 
-var subject1 = prompt("Enter the name of subject 1:");
-var subject2 = prompt("Enter the name of subject 2:");
-var subject3 = prompt("Enter the name of subject 3:");
+var subject1 = prompt("Enter The Name Of Subject 1:");
+var subject2 = prompt("Enter The Name Of Subject 2:");
+var subject3 = prompt("Enter The Name Of Subject 3:");
 
 //b) Total marks for each subject is 100, store it in another variable.
 
@@ -68,12 +68,12 @@ var totalMarks = 100;
 
 //c) Take obtained marks for first subject from user and stored it in different variable.
 
-var obtainedMarks1 = parseInt(prompt("Enter the obtained marks for " + subject1 + ":"));
+var obtainedMarks1 = parseInt(prompt("Enter The Obtained Marks For " + subject1 + ":"));
 
 //d) Take obtained marks for remaining 2 subjects from user and store them in variables.
 
-var obtainedMarks2 = parseInt(prompt("Enter the obtained marks for " + subject2 + ":"));
-var obtainedMarks3 = parseInt(prompt("Enter the obtained marks for " + subject3 + ":"));
+var obtainedMarks2 = parseInt(prompt("Enter The Obtained Marks For " + subject2 + ":"));
+var obtainedMarks3 = parseInt(prompt("Enter The Obtained Marks for " + subject3 + ":"));
 
 //e) Now calculate total marks and percentage and show the result in browser like this.(Hint: user table)
 
